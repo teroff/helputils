0249cbffecdbb5a045681dfd006a5d9a72d93a3b
Stanislav Teroff <teroff13@gmail.com>
2023-08-18 20:48:23 -0600

    Updated the main flow to use multiple exts

--- folder_organize.py
+++ folder_organize.py
@@ -1,23 +1,29 @@
 import shutil
 import sys
-from os import listdir, getcwd, path, mkdir
+from os import listdir, path, mkdir
 from getpass import getuser
 from os.path import isfile, join
 
 FILES_STRUCTURE = {
-    ["pdf, pptx"] : "Documents"
+    "Documents": "pdf pptx docx",
+    "Music": "mp3",
+    "Pictures": "jpg png gif"
 }
 
 
 def get_files(file_ext, folder_path):
     return [f for f in listdir(folder_path) if isfile(join(folder_path, f)) and f.endswith(file_ext)]
 
-def is_folder_created(folder_name: str) -> str:
-    if not path.isdir(folder_name):
-        mkdir(folder_name)
-    return folder_name
 
-def build_general_path() -> str:
+def create_folder(folder_name: str):
+    mkdir(folder_name)
+
+
+def is_folder(folder_name: str) -> bool:
+    return path.isdir(folder_name)
+
+
+def build_user_path() -> str:
     platform = sys.platform
 
     if platform == "linux" or platform == "linux2":
@@ -29,17 +35,20 @@ def build_general_path() -> str:
         # Windows...
 
 
-def move_files_to_folder(file_to_move, destination_folder):
-    shutil.move(file_to_move, is_folder_created(destination_folder))
-
-
 if __name__ == '__main__':
-    userPath = f'{build_general_path()}/Downloads'
-    for file_ext, folder in FILES_STRUCTURE.items():
-        files = get_files(file_ext, userPath)
-        if files:
-            for file in files:
-                file_path = f'{userPath}/{file}'
-                des_folder = f'{userPath}/{folder}'
-                print(file_path, des_folder)
-                move_files_to_folder(f'{userPath}/{file}', f'{userPath}/{folder}')
+    try:
+        userPath = f'{build_user_path()}/{sys.argv[1]}'
+
+        for folder, extensions in FILES_STRUCTURE.items():
+            folder = f'{userPath}/{folder}'
+            extensions = extensions.split(" ")
+            for extension in extensions:
+                files = get_files(extension, userPath)
+                if files:
+                    if not is_folder(folder): create_folder(folder)
+                    [shutil.move(f'{userPath}/{file}', folder) for file in files]
+
+    except IndexError:
+        print("You need to specify the folder where you want to file organization to happen")
+        print(f"Usage: python3 {sys.argv[0]} folder")
+        print(f"Example: python3 {sys.argv[0]} Download")
